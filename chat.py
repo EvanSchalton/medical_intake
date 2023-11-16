b5042ad3dd4b85c73d32d5d721e2790bf1df7766
Evan Schalton <Evan.Schalton@Gmail.com>
2023-11-16 04:03:48 +0000

    feat: add demographic info

--- chat.py
+++ chat.py
@@ -1,42 +1,65 @@
 import re
-import openai
+from openai import OpenAI
 from time import time, sleep
 from halo import Halo
 import textwrap
 import yaml
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+## add file handler to logger
+handler = logging.FileHandler('debugging_logs/log_%s.log' % time())
+handler.setLevel(logging.DEBUG)
+## add formatter to handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+## add handler to logger
+logger.addHandler(handler)
 
 
 ###     file operations
-
-
 def save_file(filepath, content):
     with open(filepath, 'w', encoding='utf-8') as outfile:
         outfile.write(content)
 
-
-
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8', errors='ignore') as infile:
         return infile.read()
-
-
+    
+client = OpenAI(api_key=open_file('key_openai.txt').strip())
 ###     API functions
 
 
 def chatbot(conversation, model="gpt-4-0613", temperature=0, max_tokens=2000):
+
     max_retry = 7
     retry = 0
     while True:
         try:
             spinner = Halo(text='Thinking...', spinner='dots')
             spinner.start()
+
+            logger.debug(f"conversation: {conversation}")
             
-            response = openai.ChatCompletion.create(model=model, messages=conversation, temperature=temperature, max_tokens=max_tokens)
-            text = response['choices'][0]['message']['content']
+            response = client.chat.completions.create(
+                model=model,
+                messages=conversation,
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
+
+            logger.debug(f"response: {response}")
+
+            text = response.choices[0].message.content
+            logger.debug(f"text: {text}")
 
             spinner.stop()
+
+            logger.debug(f"response.usage.total_tokens: {response.usage.total_tokens}")
             
-            return text, response['usage']['total_tokens']
+            return text, response.usage.total_tokens
         except Exception as oops:
             print(f'\n\nError communicating with OpenAI: "{oops}"')
             exit(5)
@@ -49,16 +72,61 @@ def chat_print(text):
 
 
 if __name__ == '__main__':
-    openai.api_key = open_file('key_openai.txt').strip()
+    
     
     conversation = list()
-    conversation.append({'role': 'system', 'content': open_file('system_01_intake.md')})
+    conversation.append({'role': 'system', 'content': open_file('system_00_demographics.md')})
+
     user_messages = list()
     all_messages = list()
-    print('Describe your symptoms to the intake bot. Type DONE when done.')
     
     ## INTAKE PORTION
+    print('Describe your demographic information to the intake bot. Type SUBMIT when done.')
+    standard_fields = {
+        "age (years)": "",
+        "weight (lbs)": "",
+        "height (inches)": "",
+    }
+
+    print("\n\n")
+    for field in standard_fields:
+        # get user input
+        standard_fields[field] = input(f'PATIENT: {field}: ').strip()
     
+    demo_string = ", ".join(f"{k}: {v}" for k,v in standard_fields.items())
+    text = f"About Me: {demo_string}"
+
+    user_messages.append(text)
+    all_messages.append('PATIENT: %s' % text)
+    conversation.append({'role': 'user', 'content': text})
+    response, tokens = chatbot(conversation)
+    
+    conversation.append({'role': 'assistant', 'content': response})
+    all_messages.append('DEMOGRAPHICS: %s' % response)
+    print('\n\nDEMOGRAPHICS: %s' % response)
+    
+    conversation.append({'role': 'system', 'content': open_file('system_00_demographics.md')})
+
+    print('Describe your demographics to the intake bot. Type FINISHED when you feel like you\'ve given enough information.')
+
+    while True:
+        # get user input
+        text = input('PATIENT: ').strip()
+        if text == 'FINISHED':
+            break
+        user_messages.append(text)
+        all_messages.append('PATIENT: %s' % text)
+        conversation.append({'role': 'user', 'content': text})
+        response, tokens = chatbot(conversation)
+        
+        conversation.append({'role': 'assistant', 'content': response})
+        all_messages.append('DEMOGRAPHICS: %s' % response)
+        print('\n\nDEMOGRAPHICS: %s' % response)
+
+    conversation.append({'role': 'system', 'content': open_file('system_01_intake.md')})
+
+    print('Describe your symptoms to the intake bot. Type DONE when done.')
+
     while True:
         # get user input
         text = input('\n\nPATIENT: ').strip()
@@ -68,6 +136,7 @@ if __name__ == '__main__':
         all_messages.append('PATIENT: %s' % text)
         conversation.append({'role': 'user', 'content': text})
         response, tokens = chatbot(conversation)
+        
         conversation.append({'role': 'assistant', 'content': response})
         all_messages.append('INTAKE: %s' % response)
         print('\n\nINTAKE: %s' % response)
---
+++ system_00_demographics.md
@@ -0,0 +1,5 @@
+# MISSION
+You are a patient intake chatbot focusing on demographic information. Your mission is to ask questions such as age, gender, weight and other releavant demographic information. Your chat transcript will ultimately be translated into a patient intake form.
+
+# RULES
+Ask only one question at a time. Provide some context or clarification around the follow-up questions you ask. Do not converse with the patient. 
